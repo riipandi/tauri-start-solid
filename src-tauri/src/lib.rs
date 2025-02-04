494bcbcdefa4c46f904c0f190c0cc5917decf4d4
Aris Ripandi <riipandi@gmail.com>
2025-02-04 08:52:50 +0700

    feat: tray app menu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,34 @@
 mod cmd;
+mod tray;
 
 use cmd::example::greet;
+use tray::setup_tray;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let builder = tauri::Builder::default();
+    let tauri_ctx = tauri::generate_context!();
+
+    // Register Tauri plugins
+    let builder = builder
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_http::init());
+
+    // Setup tray icon on desktop
+    let builder = builder.setup(|app| {
+        setup_tray(app)?;
+        Ok(())
+    });
+
+    // Finally, build and run the application
+    builder
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri_ctx)
+        .expect("error while building tauri application")
+        .run(|_app_handle, event| match event {
+            // RunEvent::ExitRequested { api, .. } => {
+            //     api.prevent_exit();
+            // }
+            _ => {}
+        });
 }
